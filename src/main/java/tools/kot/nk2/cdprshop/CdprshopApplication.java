8b32ba069c4cd4fd31a5b08ee2cdb1f92af8c978
nk2 <nikkoz893@gmail.com>
2023-04-08 14:42:58 +0200

    Add project template

---
+++ src/main/java/tools/kot/nk2/cdprshop/CdprshopApplication.java
@@ -0,0 +1,13 @@
+package tools.kot.nk2.cdprshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdprshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdprshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tools/kot/nk2/cdprshop/CdprshopApplicationTests.java
@@ -0,0 +1,13 @@
+package tools.kot.nk2.cdprshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdprshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
